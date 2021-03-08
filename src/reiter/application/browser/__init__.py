2a5f6dd2773c2da6561e20364642b6a723c1715b
Souheil CHELFOUH <trollfot@gmail.com>
2021-03-08 18:13:12 +0100

    Removed startup

--- src/reiter/application/browser/__init__.py
+++ src/reiter/application/browser/__init__.py
@@ -1,4 +1,6 @@
 import logging
+import inspect
+from pathlib import Path
 import chameleon.zpt.loader
 from pkg_resources import iter_entry_points
 
@@ -23,9 +25,16 @@ def tales_expressions():
 
 class TemplateLoader(chameleon.zpt.loader.TemplateLoader):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, path, ext=".pt", **kwargs):
+        path = Path(path)
+        if not path.is_absolute():
+            callerframerecord = inspect.stack()[1]
+            frame = callerframerecord[0]
+            info = inspect.getframeinfo(frame)
+            path = Path(info.filename).parent / path
+
         self.tales = tales_expressions()
-        super().__init__(*args, **kwargs)
+        super().__init__(str(path), ext, **kwargs)
 
     def load(self, filename, format=None):
         template = super().load(filename, format=format)
