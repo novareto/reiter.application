10fe94ebd64e4b61348a465405f3adde70dfdaba
Souheil Chelfouh <trollfot@gmail.com>
2021-06-09 11:46:55 +0200

    Update app.py
    Using now the reiter.application request

--- src/reiter/application/app.py
+++ src/reiter/application/app.py
@@ -7,7 +7,7 @@ import horseman.http
 import horseman.meta
 import horseman.response
 from roughrider.routing.route import Routes
-from roughrider.routing.components import RoutingRequest
+from reiter.application.request import Request
 from roughrider.events.meta import EventsCenter
 from reiter.application import registries
 from reiter.application.browser.registries import UIRegistry
@@ -39,7 +39,7 @@ class Blueprint(EventsCenter):
 class Application(Blueprint, horseman.meta.APINode):
     """Barebone application
     """
-    request_factory: RoutingRequest = field(default_factory=RoutingRequest)
+    request_factory: Request = field(default_factory=Request)
 
     def resolve(self, path, environ):
         route = self.routes.match_method(path, environ['REQUEST_METHOD'])
